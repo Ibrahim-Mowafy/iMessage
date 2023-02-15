f29540bc43a883eb99929d774d19b918ab75aa41
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-02-15 15:10:44 +0200

    Setup apollo client & apollo provider

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,16 +2,20 @@ import { theme } from '../chakra/theme';
 import { ChakraProvider } from '@chakra-ui/react';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+import { client } from '@/graphql/apollo-client';
+import { ApolloProvider } from '@apollo/client';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
