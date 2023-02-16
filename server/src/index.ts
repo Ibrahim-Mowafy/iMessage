829f5ee82ba41560bd5f3507ed45a767190a2e37
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-02-16 10:56:39 +0200

    Adding cors options in server side

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,12 +8,16 @@ import express from 'express';
 import http from 'http';
 import typeDefs from '../graphql/typeDefs';
 import resolvers from '../graphql/resolvers';
+import * as dotenv from 'dotenv';
 
 async function main() {
+  dotenv.config();
   const app = express();
   const httpServer = http.createServer(app);
 
   const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+  const corsOptions = { origin: process.env.CLIENT_ORIGIN, credentials: true };
 
   const server = new ApolloServer({
     schema,
@@ -25,7 +29,7 @@ async function main() {
     ],
   });
   await server.start();
-  server.applyMiddleware({ app });
+  server.applyMiddleware({ app, cors: corsOptions });
   await new Promise<void>((resolve) =>
     httpServer.listen({ port: 4000 }, resolve)
   );
