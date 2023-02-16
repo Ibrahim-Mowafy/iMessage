3830fa63cafc4f891b4318859ba4f03be86a7050
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-02-16 11:55:45 +0200

    Adding custom session type

---
+++ server/src/lib/next-auth.d.ts
@@ -0,0 +1,15 @@
+import 'next-auth';
+declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: User;
+  }
+
+  interface User {
+    id: string;
+    username: string;
+    image: string;
+  }
+}
