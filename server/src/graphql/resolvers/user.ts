bcd7be0ad1c3c71185aa73d60602145bd83f6a5c
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-02-16 16:24:32 +0200

    Adding createUsername Resolver Function

--- server/src/graphql/resolvers/user.ts
+++ server/src/graphql/resolvers/user.ts
@@ -1,19 +1,54 @@
-import { GraphQLContext } from './../../util/types';
+import { CreateUsernameResponse, GraphQLContext } from './../../util/types';
 const resolvers = {
   Query: {
     searchUsers: () => {},
   },
   Mutation: {
-    createUsername: (
+    createUsername: async (
       _: any,
       args: { username: string },
       context: GraphQLContext
-    ) => {
+    ): Promise<CreateUsernameResponse> => {
       const { username } = args;
       const { session, prisma } = context;
 
-      console.log('🚀 ~ file: user.ts:8 ~ username', username);
-      console.log('🚀 ~ file: user.ts:12 ~ context', context);
+      if (!session?.user) {
+        return { error: 'Not authorized' };
+      }
+      const { id: userId } = session.user;
+      try {
+        /**
+         * Check that username is not taken
+         */
+        const existingUser = await prisma.user.findUnique({
+          where: {
+            username,
+          },
+        });
+
+        if (existingUser) {
+          return {
+            error: 'Username already taken. Try another',
+          };
+        }
+
+        /**
+         * Update user
+         */
+        await prisma.user.update({
+          where: {
+            id: userId,
+          },
+          data: {
+            username,
+          },
+        });
+
+        return { success: true };
+      } catch (error: any) {
+        console.log('createUsername error', error);
+        return { error: error?.message as string };
+      }
     },
   },
 };
