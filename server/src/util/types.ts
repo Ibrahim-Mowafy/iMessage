f1cd397a7e12858044f3c172113a29d445cb13ec
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-02-16 16:25:11 +0200

    Adding User & create user response interface

--- server/src/util/types.ts
+++ server/src/util/types.ts
@@ -1,7 +1,30 @@
 import { PrismaClient } from '@prisma/client';
-import { Session } from 'next-auth';
+import { ISODateString } from 'next-auth';
+export interface Session {
+  user: User;
+  expires: ISODateString;
+}
+
 export interface GraphQLContext {
   session: Session | null;
   prisma: PrismaClient;
   //   pubsub
+}
+
+/**
+ * Users
+ */
+
+export interface User {
+  id: string;
+  username: string;
+  image: string;
+  email: string;
+  emailVerified: boolean;
+  name: string;
+}
+
+export interface CreateUsernameResponse {
+  success?: boolean;
+  error?: string;
 }
