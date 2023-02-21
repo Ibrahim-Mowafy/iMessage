5ced99fab27d75e799a0433c7f0db8295d0757c2
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-02-21 09:23:15 +0200

    Adding loading state at save button

--- client/src/components/Auth/Auth.tsx
+++ client/src/components/Auth/Auth.tsx
@@ -59,7 +59,7 @@ const Auth: React.FC<IAuthProps> = ({ session, reloadSession }) => {
               value={username}
               onChange={(event) => setUsername(event.target.value)}
             />
-            <Button width="100%" onClick={onSubmit}>
+            <Button width="100%" onClick={onSubmit} isLoading={loading}>
               Save
             </Button>
           </>
