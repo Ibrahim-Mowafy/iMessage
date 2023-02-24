e6137a319cc64a2ed1ab0b4a29037105ce3cd770
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-02-24 10:22:54 +0200

    Adding type Message

---
+++ server/src/graphql/typeDefs/messages.ts
@@ -0,0 +1,11 @@
+import { gql } from 'apollo-server-core';
+const typeDefs = gql`
+  type Message {
+    id: String
+    sender: User
+    body: String
+    createdAt: Date
+  }
+`;
+
+export default typeDefs;
