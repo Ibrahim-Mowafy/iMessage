31736efc431f22a761ab8ac9fb0e5e617d3bc663
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-02-24 10:24:02 +0200

    Implement message typedef

--- server/src/graphql/typeDefs/index.ts
+++ server/src/graphql/typeDefs/index.ts
@@ -1,6 +1,7 @@
 import userTypeDefs from './user';
 import conversationTypeDefs from './conversation';
+import messagesTypeDefs from './messages';
 
-const typeDefs = [userTypeDefs, conversationTypeDefs];
+const typeDefs = [userTypeDefs, conversationTypeDefs, messagesTypeDefs];
 
 export default typeDefs;
