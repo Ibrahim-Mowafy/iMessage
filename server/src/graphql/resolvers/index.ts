7ad4f0eb26760892405e5b4f20a7e6b4ea12d2cd
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-02-26 17:54:12 +0200

    Adding messageSent subscription & Conversation Messages query backend

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,8 +1,14 @@
 import userResolvers from './user';
 import conversationResolvers from './conversation';
+import messageResolvers from './message';
 
 import merge from 'lodash.merge';
 
-const resolvers = merge({}, userResolvers, conversationResolvers);
+const resolvers = merge(
+  {},
+  userResolvers,
+  conversationResolvers,
+  messageResolvers
+);
 
 export default resolvers;
---
+++ server/src/graphql/resolvers/message.ts
@@ -0,0 +1,171 @@
+import { Prisma } from '@prisma/client';
+import { GraphQLError } from 'graphql';
+import { withFilter } from 'graphql-subscriptions';
+import { userIsConversationParticipant } from '../../util/functions';
+import {
+  GraphQLContext,
+  MessagePopulated,
+  MessageSentSubscriptionPayload,
+  SendMessageArguments,
+} from '../../util/types';
+import { conversationPopulated } from './conversation';
+
+const resolvers = {
+  Query: {
+    messages: async (
+      _: any,
+      args: { conversationId: string },
+      context: GraphQLContext
+    ): Promise<Array<MessagePopulated>> => {
+      const { session, prisma } = context;
+      const { conversationId } = args;
+
+      if (!session?.user) {
+        throw new GraphQLError('Not authorized');
+      }
+
+      const { id: userId } = session.user;
+
+      /**
+       * Verify that conversation exists & user is a participant
+       */
+      const conversation = await prisma.conversation.findUnique({
+        where: {
+          id: conversationId,
+        },
+        include: conversationPopulated,
+      });
+
+      if (!conversation) {
+        throw new GraphQLError('Conversation Not Found');
+      }
+
+      const allowedToView = userIsConversationParticipant(
+        conversation.participants,
+        userId
+      );
+      if (!allowedToView) {
+        throw new GraphQLError('Not Authorized');
+      }
+
+      try {
+        const messages = await prisma.message.findMany({
+          where: {
+            conversationId,
+          },
+          include: messagePopulated,
+          orderBy: {
+            createdAt: 'desc',
+          },
+        });
+        return messages;
+      } catch (error: any) {
+        console.log('messages error', error);
+        throw new GraphQLError(error?.message);
+      }
+    },
+  },
+  Mutation: {
+    sendMessage: async (
+      _: any,
+      args: SendMessageArguments,
+      context: GraphQLContext
+    ): Promise<boolean> => {
+      const { session, prisma, pubsub } = context;
+      const { id: messageId, conversationId, senderId, body } = args;
+
+      if (!session?.user) {
+        throw new GraphQLError('Not authorized');
+      }
+      const { id: userId } = session.user;
+
+      if (userId !== senderId) {
+        throw new GraphQLError('Not authorized');
+      }
+      try {
+        /**
+         * create new message
+         */
+        const newMessage = await prisma.message.create({
+          data: {
+            id: messageId,
+            senderId,
+            conversationId,
+            body,
+          },
+          include: messagePopulated,
+        });
+
+        /**
+         * Update conversation entity
+         */
+        const conversation = await prisma.conversation.update({
+          where: {
+            id: conversationId,
+          },
+          data: {
+            latestMessageId: newMessage.id,
+            participants: {
+              update: {
+                where: {
+                  id: senderId,
+                },
+                data: {
+                  hasSeenLatestMessage: true,
+                },
+              },
+              updateMany: {
+                where: {
+                  NOT: {
+                    userId: senderId,
+                  },
+                },
+                data: {
+                  hasSeenLatestMessage: false,
+                },
+              },
+            },
+          },
+        });
+
+        pubsub.publish('MESSAGE_SENT', { messageSent: newMessage });
+        // pubsub.publish('CONVERSATION_UPDATED', {
+        //   conversationUpdated: { conversation },
+        // });
+      } catch (error) {
+        console.log('sendMessage error', error);
+        throw new GraphQLError('Error sending message');
+      }
+
+      return true;
+    },
+  },
+  Subscription: {
+    messageSent: {
+      subscribe: withFilter(
+        (_: any, __: any, context: GraphQLContext) => {
+          const { pubsub } = context;
+          return pubsub.asyncIterator('MESSAGE_SENT');
+        },
+        (
+          payload: MessageSentSubscriptionPayload,
+          args: { conversationId: string },
+          context: GraphQLContext
+        ) => {
+          return payload.messageSent.conversationId === args.conversationId;
+        }
+      ),
+    },
+  },
+};
+
+export const messagePopulated = Prisma.validator<Prisma.MessageInclude>()({
+  sender: {
+    select: {
+      id: true,
+      username: true,
+    },
+  },
+});
+
+export default resolvers;
