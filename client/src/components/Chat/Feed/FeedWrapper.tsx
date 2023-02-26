b088e960e28fb1678b4ffa4967ab647c13256fff
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-02-26 17:55:21 +0200

    Implement message input component at feed

--- client/src/components/Chat/Feed/FeedWrapper.tsx
+++ client/src/components/Chat/Feed/FeedWrapper.tsx
@@ -2,6 +2,7 @@ import { Flex } from '@chakra-ui/react';
 import { Session } from 'next-auth';
 import { useRouter } from 'next/router';
 import MessagesHeader from './Messages/Header';
+import MessageInput from './Messages/Input';
 
 interface FeedWrapperProps {
   session: Session;
@@ -20,14 +21,17 @@ const FeedWrapper: React.FC<FeedWrapperProps> = ({ session }) => {
       direction="column"
     >
       {conversationId && typeof conversationId === 'string' ? (
-        <Flex
-          direction="column"
-          justify="space-between"
-          overflow="hidden"
-          flexGrow={1}
-        >
-          <MessagesHeader conversationId={conversationId} userId={useId} />
-        </Flex>
+        <>
+          <Flex
+            direction="column"
+            justify="space-between"
+            overflow="hidden"
+            flexGrow={1}
+          >
+            <MessagesHeader conversationId={conversationId} userId={useId} />
+          </Flex>
+          <MessageInput session={session} conversationId={conversationId} />
+        </>
       ) : (
         <div>No Conversation Selected</div>
       )}
