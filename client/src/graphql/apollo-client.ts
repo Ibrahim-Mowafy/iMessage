9990b8774fa027e5fcaafd7601d6cf3ec0739fe6
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-03-01 22:25:27 +0200

    Adding server link deployed

--- client/src/graphql/apollo-client.ts
+++ client/src/graphql/apollo-client.ts
@@ -8,7 +8,8 @@ const wsLink =
   typeof window !== 'undefined'
     ? new GraphQLWsLink(
         createClient({
-          url: 'ws://localhost:4000/graphql/subscriptions',
+          // url: 'ws://localhost:4000/graphql/subscriptions',
+          url: 'ws://imessage-prod.up.railway.app/graphql/subscriptions',
           connectionParams: async () => ({
             session: await getSession(),
           }),
@@ -17,7 +18,8 @@ const wsLink =
     : null;
 
 const httpLink = new HttpLink({
-  uri: `http://localhost:4000/graphql`,
+  // uri: `http://localhost:4000/graphql`,
+  uri: `https://imessage-prod.up.railway.app/graphql`,
   credentials: 'include',
 });
 
